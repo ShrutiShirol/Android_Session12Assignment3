1692d34c1730bd709c921d8f81dc3470dee4450e
shrutishirol <shrutishirol3@gmail.com>
2017-03-24 00:43:58 +0530

    initial commit

---
+++ ExpandableNotification12.3/app/src/main/java/com/android/app/expandablenotification123/HandleNotificationActivity.java
@@ -0,0 +1,13 @@
+package com.android.app.expandablenotification123;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HandleNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handle_notification);
+    }
+}
